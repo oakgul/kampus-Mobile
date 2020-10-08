b2f35a5a8ea9989cf90208cc6550a6568f1cca52
oakgul <orhan.akgull@gmail.com>
2020-10-08 14:43:56 +0300

    repo init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const App = () => {
+  return (
+    <View>
+      <Text>ORHAN AKGÜL REACT NATİVE DE YAZIYOR</Text>
+      <Text>Kampus App'in Mobile Uygulaması da geliyor...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
