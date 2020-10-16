eea705ebd05d55ba309ff2d111065320820ce42e
oakgul <orhan.akgull@gmail.com>
2020-10-16 17:28:23 +0300

    dashboard profile card

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import { Component } from 'react';
-import {StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 // import Announce from './Announce';
 
 const Dashboard = () => {    
     return (
         <View style={styles.dashboard}>
+
+            <View style={styles.userCard}>
+                <View>
+                    <Image
+                        style={styles.userImage} 
+                        source={require('../assets/profile.jpg')} 
+                    /> 
+                </View>  
+                <View style={styles.userInfo}>
+                    <View>
+                        <Text style={styles.username}>Orhan Akgül</Text>
+                        <Text style={styles.userTitle}>Bilgisayar Mühendisi</Text>
+                    </View>
+
+                    <View style={styles.items}>
+                        <View style={styles.perItems}>
+                            <Text>Duyuru</Text>
+                            <Text style={styles.counter}>22</Text>
+                        </View>
+                        <View style={styles.perItems}>
+                            <Text>Görev</Text>
+                            <Text style={styles.counter}>15</Text>
+                        </View>
+                        <View style={styles.perItems}>
+                            <Text>Sıra</Text>
+                            <Text style={styles.counter}>06</Text>
+                        </View>
+                    </View>
+                </View>              
+            </View>
+
             <View style={styles.up}>
                 <TouchableOpacity 
                     style={styles.button}
@@ -42,6 +73,49 @@ const styles = StyleSheet.create({
     dashboard: {
         flex: 1,        
     },
+    userCard: {
+        // height: 200,
+        flexDirection: 'row',
+        backgroundColor: '#DAF7A6',
+        paddingHorizontal: 20,
+        paddingVertical: 20
+    },
+    userImage: {
+        marginTop:10,
+        width: 100,
+        height: 100,
+        borderRadius: 50
+        
+    },
+    username: {
+        fontSize: 20,
+    },
+    userTitle: {
+        fontSize: 15,
+        color: 'gray',
+    },
+    items: {        
+        width: 230,
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginVertical: 20,
+        backgroundColor: '#D5D8DC',
+    },
+    perItems: {
+        alignItems: 'center'
+    },
+    counter: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    userInfo: {
+        marginHorizontal: 20
+    },
+
     up:{
         flex: 1,
         flexDirection: 'row',
